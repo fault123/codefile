61a16449cd860445c9a326a55728f6688601eec7
fault123 <2444073573@qq.com>
2023-02-01 12:20:31 +0800

    ccc

--- python/exp.py
+++ python/exp.py
@@ -1,26 +1,30 @@
 # -*- coding: utf-8 -*-
-import requests,time,re
-
-url = "http://49cfdda8-bb25-4e6b-814a-8045ea321ea9.node4.buuoj.cn:81/index.php"
-req = requests.session()    #保持会话
-getRe = req.get(url)    #get请求
-getRe.encoding = "UTF-8"    #将返回的结果用UTF-8编码，不然会返回乱码
-reContent = re.findall("[0-9]* [-+*/] [0-9]*",getRe.text)   #提取表达式。math和search正则只会匹配一次，findall会一直匹配
-formula = ''.join(reContent).replace(" ", "")   #提取表达式
-comResult = eval(formula)   #eval执行一个表达式
-print(comResult)    
-data = {"answer":comResult}
-postRe = req.post(url,data)
-postRe.encoding = "UTF-8"
-print(postRe.text)
-
-for i in range(1005):   #多运行几次，避免其中的请求失败
-    reContent = re.findall("[0-9]* [-+*/] [0-9]*",postRe.text)
-    formula = ''.join(reContent).replace(" ", "")
-    comResult = eval(formula)
-    print(comResult)
-    data = {"answer":comResult}
-    postRe = req.post(url,data)
-    postRe.encoding = "UTF-8"
-    time.sleep(0.1)		#请求太快会被禁掉
-    print(postRe.text) 
+from requests import *
+import time 
+import re
+url1="http://49cfdda8-bb25-4e6b-814a-8045ea321ea9.node4.buuoj.cn:81/"
+url2="http://49cfdda8-bb25-4e6b-814a-8045ea321ea9.node4.buuoj.cn:81/index.php"
+s=session()
+ss=s.get(url1).text
+sss=re.findall("[0-9]* [-+*/] [0-9]*",ss)
+sss=''.join(sss).replace(" ","")
+answer=eval(sss)
+data={
+    'answer':answer
+}
+res=s.post(url2,data)
+res.encoding="UTF-8"
+print(res.text)
+time.sleep(0.1)
+for i in range(1003):
+    sss=re.findall("[0-9]* [-+*/] [0-9]*",res.text)
+    sss=''.join(sss).replace(" ","")
+    answer=eval(sss)
+    print(answer)
+    data={
+        'answer':answer
+    }
+    res=s.post(url=url2,data=data)
+    res.encoding="UTF-8"
+    time.sleep(0.1)
+    print(res.text)
